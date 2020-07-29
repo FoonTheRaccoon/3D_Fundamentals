b5a618d87108f69c9587d8881f8c68319f346769
Foon <projectraccoon@mailbox.org>
2020-07-29 14:53:15 -0500

    Need to rework Pixel shader to use texture ptr and build vertex shader.

---
+++ Engine/VertexShader.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Vertex.h"
+
+class Vertex_Shader
+{
+public:
+	//virtual Vertex Effect()
+	//{
+	//
+	//}
+};
+
+
+namespace VertexShader
+{
+	inline Vertex_Shader Default;
+}
