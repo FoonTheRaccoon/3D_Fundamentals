f6bbd6c255289eebd0e7c11098026f7d43e18277
Foon <projectraccoon@mailbox.org>
2020-09-10 06:59:04 -0500

    Added specular hightlights to the smooth shading

--- Engine/Light.h
+++ Engine/Light.h
@@ -38,41 +38,42 @@ public:
 			pos.z = pos.z - speed * dt;
 		}
 	}
-	float SetPixelLightShadingBias(const Vertex& vert)
+	float SetPixelLightShadingBias(const Vertex& vert) //Smooth Shading
 	{
+		const Vec3 surf_norm = vert.norm.GetNormalized();
+
 		// vertex to light data
 		const Vec3 v_to_l = pos - vert.worldPos;
 		const float dist = v_to_l.Len();
 		const Vec3 dir = v_to_l / dist;
+
 		// calculate attenuation
 		const float attenuation = 1.0f /
-			(constant_attenuation + linear_attenuation * dist * quadradic_attenuation * sq(dist));
-		// calculate intensity based on angle of incidence and attenuation
-		const float d = diffuse * attenuation;
+			(constant_attenuation + linear_attenuation * dist + quadradic_attenuation * sq(dist));
 
-		float result = (vert.norm.GetNormalized() * dir) * intensity * d;
+		const float result = (surf_norm * dir) * intensity * attenuation;
 
-		result = std::clamp(result, ambient, diffuse);
+		const Vec3 w = surf_norm * (v_to_l * surf_norm);
+		const Vec3 r = w * 2.0f - v_to_l;
+		// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
+		const float s = specular_intensity * std::pow(std::max(0.0f, -r.GetNormalized() * vert.worldPos.GetNormalized()), specular_power);
 
-		return result;
+		return std::clamp(result + s, ambient, diffuse);
 	}
-	float SetPixelLightShadingBias(const Triangle& tri)
+	float SetPixelLightShadingBias(const Triangle& tri) //Flat Shading
 	{
 		// vertex to light data
 		const Vec3 v_to_l = pos - tri.triCenter;
 		const float dist = v_to_l.Len();
 		const Vec3 dir = v_to_l / dist;
+
 		// calculate attenuation
 		const float attenuation = 1.0f /
-			(constant_attenuation + linear_attenuation * dist * quadradic_attenuation * sq(dist));
-		// calculate intensity based on angle of incidence and attenuation
-		const float d = diffuse * attenuation;
+			(constant_attenuation + linear_attenuation * dist + quadradic_attenuation * sq(dist));
 
-		float result = (tri.faceNorm * dir) * intensity * d;
+		const float result = (tri.faceNorm * dir) * intensity * attenuation;
 
-		result = std::clamp(result, ambient, diffuse);
-
-		return result;
+		return std::clamp(result, ambient, diffuse);
 	}
 	const std::vector<Triangle>& GetTriangles() const
 	{
@@ -87,13 +88,18 @@ public:
 		return &model.texture;
 	}
 private:
-	float intensity = 1.0f;
-	float ambient = 0.05f;
-	float diffuse = 0.9f;
+	float intensity = 2.0f;
+	float ambient = 0.01f;
+	float diffuse = 0.95f;
 	float speed = 1.0f;
+	//Diffuse
 	float linear_attenuation = 1.0f;
-	float quadradic_attenuation = 1.5f;
-	float constant_attenuation = 1.0f;
+	float quadradic_attenuation = 2.5f;
+	float constant_attenuation = 0.3f;
+	//Specular
+	float specular_power = 60.0f;
+	float specular_intensity = 0.6f;
+
 	Vec3 pos = { 0.0f, 0.0f , 1.0f};
 	Model model;
 };
